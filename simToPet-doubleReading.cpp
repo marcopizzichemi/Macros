57a1fea15c03feb56a01730609c1287d72448466
irenecortinovis <irenecortinovis1@gmail.com>
2016-08-24 17:18:42 +0200

    added cross section and lambda data, completed calculation for the probabilities

--- simToPet-doubleReading.cpp
+++ simToPet-doubleReading.cpp
@@ -26,39 +26,44 @@
 
 
 //travel function used for 511 gamma rays that reach interaction point
-Float_t simTravel1(Float_t pathLength)
+//pathlength and lambda511 in cm
+Float_t simTravel1(Float_t pathLength, Float_t lambda511)
 {
-  Float_t probTravel1 = 1;
+  Float_t probTravel1 = exp(-pathLength/lambda511);
   return probTravel1;
 }
 
-//travel function used between compton and photoelectric effects
-Float_t simTravel2(Float_t pathLength, Float_t lambda)
+//compton effect function
+Float_t simCompton(Float_t comptonAngle)
 {
-  Float_t probTravel2 = 1;
+  Float_t finalEnergy = 0.511/(2-cos(comptonAngle));
+  //std::cout << "energy calculated with angle: " << finalEnergy << std::endl;
+  Float_t probCompton = pow((finalEnergy/0.511),2)*(0.511/finalEnergy + finalEnergy/0.511 - pow(sin(comptonAngle),2));
+  return probCompton;
+}
+
+//travel function used between compton and photoelectric effects
+//energy is 511-energy deposited in first crystal because of compton effect, in keV
+//pathLength and lambdaE in cm
+Float_t simTravel2(Float_t pathLength, Float_t lambdaE)
+{
+  Float_t probTravel2 = exp(-pathLength/lambdaE)/lambdaE;
   return probTravel2;
 }
 
 //photoelectric effect function
-Float_t simPhotoelectric(Float_t preEnergy)
+//energy is energy deposited in second crystal, in keV
+Float_t simPhotoelectric(Float_t csPE)
 {
-  Float_t probPhotoelectric = 1;
+  Float_t probPhotoelectric = csPE;
   return probPhotoelectric;
 }
 
-//compton effect function
-Float_t simCompton(Float_t energyDeposited, Float_t comptonAngle)
-{
-  Float_t probCompton = 1;
-  return probCompton;
-}
 
-//distance  between 2 points in 3d space
+//distance between 2 points in 3d space
 Float_t distance3D(Float_t ax, Float_t ay, Float_t az, Float_t bx, Float_t by, Float_t bz)
 {
-  //vector v
   Float_t v[3] = {bx-ax, by-ay, bz-az};
-  //modulus vector
   Float_t vMod = sqrt(pow(v[0],2) + pow(v[1],2) + pow(v[2],2));
   return vMod;
 }
@@ -67,18 +72,12 @@ Float_t distance3D(Float_t ax, Float_t ay, Float_t az, Float_t bx, Float_t by, F
 //angle between 3 points in 3d space
 Float_t angle3D(Float_t ax, Float_t ay, Float_t az, Float_t bx, Float_t by, Float_t bz, Float_t cx, Float_t cy, Float_t cz)
 {
-  //vectors v1 and v2
   Float_t v1[3] = {bx-ax, by-ay, bz-az};
   Float_t v2[3] = {cx-bx, cy-by, cz-bz};
-  //normalize vectors
   Float_t v1Mod = sqrt(pow(v1[0],2) + pow(v1[1],2) + pow(v1[2],2));
-  Float_t v1norm[3] = {v1[0]/v1Mod, v1[1]/v1Mod, v1[2]/v1Mod};
   Float_t v2Mod = sqrt(pow(v2[0],2) + pow(v2[1],2) + pow(v2[2],2));
-  Float_t v2norm[3] = {v2[0]/v2Mod, v2[1]/v2Mod, v2[2]/v2Mod};
-  //dot product
-  Float_t dotProduct = v1norm[0] * v2norm[0] + v1norm[1] * v2norm[1] + v1norm[2] * v2norm[2];
-  //angle
-  Float_t angle = acos(dotProduct);
+  Float_t dotProduct = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+  Float_t angle = acos(dotProduct/(v1Mod*v2Mod));
   return angle;
 }
 
@@ -98,10 +97,7 @@ int main (int argc, char** argv)
     std::cout << "Adding file " << argv[i] << std::endl;
     tree->Add(argv[i]);
   }
-  
 
-  //play with input names
-  //std::string inputFileName = 
   
   //find the number of channels directly from the tchain file
   //before creating the variables
@@ -266,7 +262,12 @@ int main (int argc, char** argv)
   t1->Branch("NumbOfInteractions",&NumbOfInteractions,"NumbOfInteractions/S"); 
 
   //create canvas
-  TCanvas* Canvas = new TCanvas("Canvas", "Canvas", 1200, 800); 
+  TCanvas* Canvas1 = new TCanvas("Canvas1", "Canvas1", 1200, 800); 
+  TCanvas* Canvas2 = new TCanvas("Canvas2", "Canvas2", 1200, 800); 
+  TCanvas* Canvas3 = new TCanvas("Canvas3", "Canvas3", 1200, 800); 
+  TCanvas* Canvas4 = new TCanvas("Canvas4", "Canvas4", 1200, 800); 
+  //TCanvas* Canvas5 = new TCanvas("Canvas5", "Canvas5", 1200, 800); 
+
 
 
   TH2F* DOIscatter = new TH2F ("DOIscatter", "DOIscatter", 50, 0, 15, 50, 8, -8);
@@ -277,13 +278,83 @@ int main (int argc, char** argv)
   TGraph* conversionNphotonsEnergy = new TGraph();
   conversionNphotonsEnergy->SetNameTitle ("Conversion Edep vs detector hits", "Conversion Energy vs N photons detected");
 
-
   
+ 
   long int counter = 0;
   long int pointN = 0;
   long int goodCounter = 0;
   long int winCounter = 0;
 
+
+  Float_t vEnergyComptonPhotoelLYSO[103] = 
+  {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100,
+  105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200,
+  205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 295, 300,
+  305, 310, 315, 320, 325, 330, 335, 340, 345, 350, 355, 360, 365, 370, 375, 380, 385, 390, 395, 400,
+  405, 410, 415, 420, 425, 430, 435, 440, 445, 450, 455, 460, 465, 470, 475, 480, 485, 490, 495, 500,
+  505, 510, 515};
+
+  Float_t vCSComptonLYSO[103] = 
+  {3.9897,6.5516,8.0894,9.1666,9.9455,10.505,10.902,11.18,11.37,11.494,11.568,11.606,11.615,11.603,
+  11.574,11.534,11.484,11.427,11.365,11.299,11.23,11.16,11.088,11.015,10.942,10.868,10.796,10.723,
+  10.651,10.58,10.509,10.44,10.371,10.303,10.236,10.17,10.105,10.041,9.9781,9.9161,9.855,9.7949,9.7357,
+  9.6774,9.62,9.5635,9.5079,9.4532,9.3992,9.3461,9.2938,9.2423,9.1915,9.1415,9.0922,9.0436,8.9957,
+  8.9485,8.9019,8.856,8.8107,8.7661,8.722,8.6786,8.6357,8.5934,8.5516,8.5104,8.4697,8.4296,8.3899,
+  8.3507,8.312,8.2738,8.2361,8.1988,8.162,8.1255,8.0896,8.054,8.0189,7.9841,7.9498,7.9158,7.8822,7.849,
+  7.8162,7.7837,7.7515,7.7197,7.6883,7.6572,7.6264,7.5959,7.5658,7.5359,7.5064,7.477,7.4482,7.4195,
+  7.3912,7.363,7.3353};
+
+  Float_t vCSPhotoelectricLYSO[103] =
+  {417.63,169.13,93.706,43.83,23.927,14.502,9.4737,6.5458,4.724,3.5298,2.7133,2.1355,
+  8.9356,7.3898,6.1779,5.216,4.4434,3.8162,3.302,2.8766,2.5217,2.2233,1.9706,1.7552,1.5704,1.411,
+  1.2728,1.1524,1.0469,0.95419,0.87231,0.79973,0.73516,0.67752,0.6259,0.57953,0.53776,0.50002,
+  0.46584,0.4348,0.40655,0.38077,0.35721,0.33562,0.31581,0.29759,0.28079,0.2653,0.25097,0.2377,
+  0.2254,0.21397,0.20334,0.19344,0.1842,0.17558,0.16752,0.15997,0.15289,0.14625,0.14002,0.13415,
+  0.12863,0.12343,0.11853,0.1139,0.10952,0.10538,0.10147,0.097761,0.094244,0.090909,0.087741,0.084732,
+  0.08187,0.079147,0.076554,0.074083,0.071728,0.06948,0.067335,0.065285,0.063327,0.061453,0.059661,
+  0.057945,0.056301,0.054725,0.053214,0.051765,0.050374,0.049037,0.047754,0.046519,0.045333,0.044191,
+  0.043092,0.042034,0.041014,0.040025,0.039078,0.038164,0.037282};
+
+  for(int i=0; i<103; i++)
+    vCSPhotoelectricLYSO[i]=vCSPhotoelectricLYSO[i]*100;
+ 
+  Float_t vEnergyLambdaLYSO[23] =
+  {0.001,0.0015,0.002,0.003,0.004,0.005,0.006,0.008,0.01,0.015,0.02,
+  0.03,0.04,0.05,0.06,0.08,0.1,0.15,0.2,0.3,0.4,0.5,0.6};
+ 
+  Float_t vLambdaLYSO[23] =
+  {4.0546e-05,9.7126e-05,4.6136e-05,9.6224e-05,0.00019359,0.00033754,
+  0.00053432,0.0011114,0.00078548,0.0014043,0.0029794,0.008657,0.018449,
+  0.032981,0.052543,0.024507,0.043318,0.11986,0.23536,0.53397,0.84433,
+  1.1232,1.6516};
+
+
+
+  TGraph* comptonCrossSectionLYSO = new TGraph(103, vEnergyComptonPhotoelLYSO, vCSComptonLYSO);
+  comptonCrossSectionLYSO->SetNameTitle ("Gamma energy vs Compton cross section in LYSO","Gamma energy vs Compton cross section in LYSO");
+ 
+  TGraph* photoelectricCrossSectionLYSO = new TGraph(103, vEnergyComptonPhotoelLYSO, vCSPhotoelectricLYSO);
+  photoelectricCrossSectionLYSO->SetNameTitle ("Gamma energy vs photoelectric cross section in LYSO","Gamma energy vs photoelectric cross section in LYSO");
+ 
+  TGraph* lambdaLYSO = new TGraph(23, vEnergyLambdaLYSO, vLambdaLYSO);
+  lambdaLYSO->SetNameTitle ("Gamma energy vs lambda in LYSO","Gamma energy vs lambda in LYSO");
+
+  Canvas2->cd();
+  comptonCrossSectionLYSO->Draw("A*");
+  comptonCrossSectionLYSO->GetXaxis()->SetTitle("gamma energy [keV]");
+  comptonCrossSectionLYSO->GetYaxis()->SetTitle("cross section [mm2/g]");
+
+  Canvas3->cd();
+  photoelectricCrossSectionLYSO->Draw("A*");
+  photoelectricCrossSectionLYSO->GetXaxis()->SetTitle("gamma energy [keV]");
+  photoelectricCrossSectionLYSO->GetYaxis()->SetTitle("cross section [mm2/g]");
+
+  Canvas4->cd();
+  lambdaLYSO->Draw("A*");
+  lambdaLYSO->GetXaxis()->SetTitle("gamma energy [MeV]");
+  lambdaLYSO->GetYaxis()->SetTitle("cross lambda [cm]");
+
+ 
 
 
   int nEntries = tree->GetEntries();
@@ -436,8 +507,8 @@ int main (int argc, char** argv)
     goodInteractionsX = new Float_t [NumbOfGoodInteractions];  
     Float_t  *goodInteractionsY; //will store the Y position of the energy deposited in crystals where good interactions happened
     goodInteractionsY = new Float_t [NumbOfGoodInteractions];  
-    Float_t  *goodInteractionsDOI; //will store the DOI of the energy deposited in crystals where good interactions happened
-    goodInteractionsDOI = new Float_t [NumbOfGoodInteractions];  
+    Float_t  *goodInteractionsZ; //will store the DOI of the energy deposited in crystals where good interactions happened
+    goodInteractionsZ = new Float_t [NumbOfGoodInteractions];  
     Float_t  *goodInteractionsTime; //will store the time of the first interaction in crystals where good interactions happened (problem: the gamma ray can go back to the first crystal)
     goodInteractionsTime = new Float_t [NumbOfGoodInteractions];  
 
@@ -459,7 +530,7 @@ int main (int argc, char** argv)
         goodCrystals[NumbOfGoodInteractions]=i;
         goodInteractionsX[NumbOfGoodInteractions]=wX/totEnergyCry;
         goodInteractionsY[NumbOfGoodInteractions]=wY/totEnergyCry;
-        goodInteractionsDOI[NumbOfGoodInteractions]=wZ/totEnergyCry;
+        goodInteractionsZ[NumbOfGoodInteractions]=wZ/totEnergyCry;
         goodInteractionsTime[NumbOfGoodInteractions] = pTime[i]->at(0); //time of first interaction in the crystal
       }
       totEnergyCry = 0;
@@ -472,7 +543,6 @@ int main (int argc, char** argv)
     //filter events with exactly 2 good interactions whose energy sums up to >totalThreshold
     //the numbers of the two crystals are stored in goodCrystals[1] and goodCrystals[2]
     //the energies deposited in the two crystals are stored in goodInteractionsEnergy[1] and goodInteractionsEnergy[2]
-    //the DOIs are stored in goodInteractionsDOI[1] and goodInteractionsDOI[2]
     //..
     Float_t comptonAngle1;
     Float_t comptonAngle2;
@@ -483,33 +553,27 @@ int main (int argc, char** argv)
     {
       goodCounter++;
       //compton angles in the two cases
-      comptonAngle1 = angle3D(0,0,-208.2, goodInteractionsX[1], goodInteractionsY[1], goodInteractionsDOI[1], goodInteractionsX[2], goodInteractionsY[2], goodInteractionsDOI[2]);
-      comptonAngle2 = angle3D(0,0,-208.2, goodInteractionsX[2], goodInteractionsY[2], goodInteractionsDOI[2], goodInteractionsX[1], goodInteractionsY[1], goodInteractionsDOI[1]);
+      comptonAngle1 = M_PI-angle3D(0,0,-208.2, goodInteractionsX[1], goodInteractionsY[1], goodInteractionsZ[1], goodInteractionsX[2], goodInteractionsY[2], goodInteractionsZ[2]);
+      comptonAngle2 = M_PI-angle3D(0,0,-208.2, goodInteractionsX[2], goodInteractionsY[2], goodInteractionsZ[2], goodInteractionsX[1], goodInteractionsY[1], goodInteractionsZ[1]);
       //distance travelled between compton and photoelectric effect
-      comptonPhotoelDistance = distance3D(goodInteractionsX[1], goodInteractionsY[1], goodInteractionsDOI[1], goodInteractionsX[2], goodInteractionsY[2], goodInteractionsDOI[2]);
-      //std::cout << "crystal 1: " << goodCrystals[1] << " crystal2: " << goodCrystals[2] << "\n DOI1: " << goodInteractionsDOI[1] << " DOI2: " << goodInteractionsDOI[2] <<  "\n angle1: " << comptonAngle0 << " angle 2: " << comptonAngle1 << "\n distance: " << comptonPhotoelDistance << "\n" << std::endl;
-      //coefficients for compton scattering, function of the energy of the gamma
-      Float_t lambda1;
-      Float_t lambda2;
-      Float_t totalProbability1 = simTravel1(goodInteractionsDOI[1])*
-                          simCompton(goodInteractionsEnergy[1],comptonAngle1)* //or find delta energy?
-                          simTravel2(comptonPhotoelDistance, lambda1)*
-                          simPhotoelectric(goodInteractionsEnergy[2]);
-      Float_t totalProbability2 = simTravel1(goodInteractionsDOI[2])*
-                          simCompton(goodInteractionsEnergy[2],comptonAngle2)* //or find delta energy?
-                          simTravel2(comptonPhotoelDistance, lambda2)*
-                          simPhotoelectric(goodInteractionsEnergy[1]);
-
-      if(totalProbability1/totalProbability2 > 1 && goodInteractionsTime[1] > goodInteractionsTime[2] ||
-         totalProbability1/totalProbability2 < 1 && goodInteractionsTime[1] < goodInteractionsTime[2])
+      comptonPhotoelDistance = distance3D(goodInteractionsX[1], goodInteractionsY[1], goodInteractionsZ[1], goodInteractionsX[2], goodInteractionsY[2], goodInteractionsZ[2]);
+      //std::cout << "crystal 1 energy: " << goodInteractionsEnergy[1] << " crystal2 energy: " << goodInteractionsEnergy[2] << "\n DOI1: " << goodInteractionsZ[1] << " DOI2: " << goodInteractionsZ[2] <<  "\n angle1: " << comptonAngle1 << " angle 2: " << comptonAngle2 << "\n distance: " << comptonPhotoelDistance << "\n" << std::endl;
+      Float_t totalProbability1 = simTravel1((-7.5+goodInteractionsZ[1])/10, lambdaLYSO->Eval(511))*
+                          simCompton(comptonAngle1)*
+                          simTravel2(comptonPhotoelDistance/10, lambdaLYSO->Eval(1000*(0.511/(2-cos(comptonAngle1)))))*
+                          simPhotoelectric(photoelectricCrossSectionLYSO->Eval(1000*(0.511/(2-cos(comptonAngle1)))));
+                          //std::cout << "energy deposited in crystal: " << 0.511 - goodInteractionsEnergy[1] << std::endl;
+      Float_t totalProbability2 = simTravel1((-7.5+goodInteractionsZ[2])/10, lambdaLYSO->Eval(511))*
+                          simCompton(comptonAngle2)*
+                          simTravel2(comptonPhotoelDistance/10, lambdaLYSO->Eval(1000*(0.511/(2-cos(comptonAngle2)))))*
+                          simPhotoelectric(photoelectricCrossSectionLYSO->Eval(1000*(0.511/(2-cos(comptonAngle2)))));
+                          //std::cout << "energy deposited in crystal: " << 0.511 - goodInteractionsEnergy[2] << "\n" << std::endl;
+      if(totalProbability1/totalProbability2 > 1 && goodInteractionsTime[1] < goodInteractionsTime[2] ||
+         totalProbability1/totalProbability2 < 1 && goodInteractionsTime[1] > goodInteractionsTime[2])
       {
         winCounter++;
       }
     }
-
-
-
-
 
 
 
@@ -540,8 +604,9 @@ int main (int argc, char** argv)
   
 
   DOIscatter->Write();
+
   
-  Canvas->cd();
+  Canvas1->cd();
   TF1* line = new TF1 ("line", "[0]*x", 0, 1);
   conversionNphotonsEnergy->Fit("line", "Q");
   //conversion factor number of photons on detector and energy 
@@ -550,9 +615,16 @@ int main (int argc, char** argv)
   conversionNphotonsEnergy->GetXaxis()->SetTitle("Energy Dedposited");
   conversionNphotonsEnergy->GetYaxis()->SetTitle("N of detector hits");
   conversionNphotonsEnergy->Write();
-  
 
+  comptonCrossSectionLYSO->Write();
+  photoelectricCrossSectionLYSO->Write();
+  lambdaLYSO->Write();
+
+
+
+  
   t1->Write();
+
   fOut->Close();
   
   return 0;
