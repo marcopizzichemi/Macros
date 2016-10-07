caa9aa08c4b6666f6b742beb3aa11b68b09569d9
marcopizzichemi <marco.pizzichemi@gmail.com>
2016-10-07 18:28:44 +0200

    simRead Struct tests ongoing, implemented average w

--- simRead_struct.cpp
+++ simRead_struct.cpp
@@ -7,6 +7,7 @@
 #include "TTree.h"
 #include "TFile.h"
 #include "TChain.h"
+#include "TH2F.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -28,29 +29,29 @@ int main (int argc, char** argv)
 
   //HACK to use the dictionary easily
   std::string fullFileName = "";
-    // Code taken from: http://www.gamedev.net/community/forums/topic.asp?topic_id=459511
-    std::string path = "";
-    pid_t pid = getpid();
-    char buf[20] = {0};
-    sprintf(buf,"%d",pid);
-    std::string _link = "/proc/";
-    _link.append( buf );
-    _link.append( "/exe");
-    char proc[512];
-    int ch = readlink(_link.c_str(),proc,512);
-    if (ch != -1) {
-        proc[ch] = 0;
-        path = proc;
-        std::string::size_type t = path.find_last_of("/");
-        path = path.substr(0,t);
-    }
-    fullFileName = path + std::string("/");
-    //now even worse, assuming the executable is in build and the .C macro in code
-    // std::string command = ".L " + fullFileName.substr(0,fullFileName.size()-7) + "/code/structDictionary.C+";
-    std::string command = ".L " + fullFileName + "structDictionary.C+";
-    // std::cout << fullFileName << std::endl;
-    // std::cout << "command " << command << std::endl;
-    gROOT->ProcessLine(command.c_str());
+  // Code taken from: http://www.gamedev.net/community/forums/topic.asp?topic_id=459511
+  std::string path = "";
+  pid_t pid = getpid();
+  char buf[20] = {0};
+  sprintf(buf,"%d",pid);
+  std::string _link = "/proc/";
+  _link.append( buf );
+  _link.append( "/exe");
+  char proc[512];
+  int ch = readlink(_link.c_str(),proc,512);
+  if (ch != -1) {
+    proc[ch] = 0;
+    path = proc;
+    std::string::size_type t = path.find_last_of("/");
+    path = path.substr(0,t);
+  }
+  fullFileName = path + std::string("/");
+  //now even worse, assuming the executable is in build and the .C macro in code
+  // std::string command = ".L " + fullFileName.substr(0,fullFileName.size()-7) + "/code/structDictionary.C+";
+  std::string command = ".L " + fullFileName + "structDictionary.C+";
+  // std::cout << fullFileName << std::endl;
+  // std::cout << "command " << command << std::endl;
+  gROOT->ProcessLine(command.c_str());
 
 
   //-------------------
@@ -82,9 +83,9 @@ int main (int argc, char** argv)
   {
     //     leavesName.push_back(leavescopy->At(i)->GetName());
     if (!leavesName[i].compare(0, det_prefix.size(), det_prefix))
-      numOfCh++;
+    numOfCh++;
     // if (!leavesName[i].compare(0, cry_prefix.size(), cry_prefix))
-      // numOfCry++;
+    // numOfCry++;
   }
   //the string "cry" appears 4 times per crystal..
   // numOfCry = numOfCry / 4;
@@ -263,29 +264,458 @@ int main (int argc, char** argv)
   // tree->SetBranchAddress("PhotonEnergy",&pPhotonEnergy);
 
 
+
+  //output ttree
+  // long long int DeltaTimeTag,ExtendedTimeTag;
+  // Short_t charge[32]; //adc type is always 32 channels
+  // Float_t RealX,RealY,RealZ;
+  // Short_t CrystalsHit;
+  // Short_t NumbOfInteractions;
+  //
+  // TTree* t1 = new TTree("adc","adc");
+  //
+  // t1->Branch("ExtendedTimeTag",&ExtendedTimeTag,"ExtendedTimeTag/l"); 	//absolute time tag of the event
+  // t1->Branch("DeltaTimeTag",&DeltaTimeTag,"DeltaTimeTag/l"); 			//delta time from previous event
+  // //branches of the 32 channels data
+  // for (int i = 0 ; i < 32 ; i++)
+  // {
+  //   //empty the stringstreams
+  //   std::stringstream snames,stypes;
+  //   charge[i] = 0;
+  //   snames << "ch" << i;
+  //   stypes << "ch" << i << "/S";
+  //   t1->Branch(snames.str().c_str(),&charge[i],stypes.str().c_str());
+  // }
+  // t1->Branch("RealX",&RealX,"RealX/F");
+  // t1->Branch("RealY",&RealY,"RealY/F");
+  // t1->Branch("RealZ",&RealZ,"RealZ/F");
+  // t1->Branch("CrystalsHit",&CrystalsHit,"CrystalsHit/S");
+  // t1->Branch("NumbOfInteractions",&NumbOfInteractions,"NumbOfInteractions/S");
+  long long int DeltaTimeTag,ExtendedTimeTag;
+  Short_t charge[16];
+
+  //   TTree* t1 = new TTree("adc","adc");
+  //
+  //   t1->Branch("ExtendedTimeTag",&ExtendedTimeTag,"ExtendedTimeTag/l"); 	//absolute time tag of the event
+  //   t1->Branch("DeltaTimeTag",&DeltaTimeTag,"DeltaTimeTag/l"); 			//delta time from previous event
+  //   //branches of the 32 channels data
+  //   for (int i = 0 ; i < 32 ; i++)
+  //   {
+  //     //empty the stringstreams
+  //     std::stringstream snames,stypes;
+  //     charge[i] = 0;
+  //     snames << "ch" << i;
+  //     stypes << "ch" << i << "/S";
+  //     t1->Branch(snames.str().c_str(),&charge[i],stypes.str().c_str());
+  //   }
+
+  // correct one
+  Double_t xmppc[16]={-4.8,-1.6,1.6,4.8,-4.8,-1.6,1.6,4.8,-4.8,-1.6,1.6,4.8,-4.8,-1.6,1.6,4.8};
+  Double_t ymppc[16]={-4.8,-4.8,-4.8,-4.8,-1.6,-1.6,-1.6,-1.6,1.6,1.6,1.6,1.6,4.8,4.8,4.8,4.8};
+
+  // wrong geometry
+  //   Double_t xmppc[32]={-4.65,0,-1.55,0,1.55,0,4.65,0,-4.65,0,-1.55,0,1.55,0,4.65,0,-4.65,0,-1.55,0,1.55,0,4.65,0,-4.65,0,-1.55,0,1.55,0,4.65,0};
+  //   Double_t ymppc[32]={-4.65,0,-4.65,0,-4.65,0,-4.65,0,-1.55,0,-1.55,0,-1.55,0,-1.55,0,1.55,0,1.55,0,1.55,0,1.55,0,4.65,0,4.65,0,4.65,0,4.65,0};
+
+
+  TH2F *flood = new TH2F("FloodHisto","FloodHisto",1000,-7,7,1000,-7,7);
+  flood->GetXaxis()->SetTitle("X [mm]");
+  flood->GetYaxis()->SetTitle("Y [mm]");
+  flood->GetZaxis()->SetTitle("N");
+  //   recClean->SetTitle("Reconstruction of entire dataset");
+  //   varStream << "FloodY_" << k << ":FloodX_" << k << ">>" << histoString ;
+
+  // TH2F *positions = new TH2F("Positions","Positions",1000,-7,7,1000,-7,7);
+  // positions->GetXaxis()->SetTitle("X [mm]");
+  // positions->GetYaxis()->SetTitle("Y [mm]");
+  // positions->GetZaxis()->SetTitle("N");
+
+
+  // TH2F *HitPositions = new TH2F("HitPositions","HitPositions",1000,-7,7,1000,-7,7);
+  // HitPositions->GetXaxis()->SetTitle("X [mm]");
+  // HitPositions->GetYaxis()->SetTitle("Y [mm]");
+  // HitPositions->GetZaxis()->SetTitle("N");
+  double columsum = 0;
+  double rowsum = 0;
+  double total = 0;
+  double floodx = 0;
+  double floody = 0;
+  double floodz = 0;
+
+
+  // TBranch *b_floodx = tree->Branch("FloodX",&floodx,"floodx/F");
+  // TBranch *b_floody = tree->Branch("FloodY",&floody,"floody/F");
+  // TBranch *b_floodz = tree->Branch("FloodZ",&floodz,"floodz/F");
+
   //----------------------------------------//
   //             LOOP ON EVENTS             //
   //----------------------------------------//
   long int counter = 0;
   int nEntries = tree->GetEntries();
   std::cout << "nEntries = " << nEntries << std::endl;
+
+  TH2F *averageZvsRatio = new TH2F("AverageZ vs Ratio","AverageZ vs Ratio",100,0,1,100,-8,8);
+  averageZvsRatio->GetXaxis()->SetTitle("Ratio (Fi/(Fi+Bi))");
+  averageZvsRatio->GetYaxis()->SetTitle("AverageZ [mm]");
+
+  TH2F *wivsRatio = new TH2F("wi vs Ratio","wi vs Ratio",100,0,1,100,0,1);
+  wivsRatio->GetXaxis()->SetTitle("Ratio (Fi/(Fi+Bi))");
+  wivsRatio->GetYaxis()->SetTitle("Wi");
+
+  TH2F *wMeasuredvsRatioW = new TH2F("wMeasuredvsRatioW","wMeasuredvsRatioW",100,0,1,100,0,1);
+  wMeasuredvsRatioW->GetXaxis()->SetTitle("RatioW Sum_i(Wi*Ei/Etot)");
+  wMeasuredvsRatioW->GetYaxis()->SetTitle("Measured W");
+
+  TH2F *averageZvsWi = new TH2F("AverageZ vs Wi","AverageZ vs Wi",100,0,1,100,-8,8);
+  averageZvsWi->GetXaxis()->SetTitle("Wi");
+  averageZvsWi->GetYaxis()->SetTitle("AverageZ [mm]");
+
+  // for(int i =0 ; i < 2; i++)
+  // {
+  //   std::stringstream name;
+  //   name << "AverageZ vs. (Fi/(Fi+Bi)) - Crystal " << i
+  //   averageZvsRatio[i] = new TH2F()
+  // }
+  long int foundCandidate = 0;
+  long int singleCounter = 0;
+  long int doubleCounter = 0;
+  long int tripleCounter = 0;
+  long int multipleCounter = 0;
   for(int iEvent = 0; iEvent < nEntries ; iEvent++)
   {
     tree->GetEvent(iEvent);
-
-    std::cout << "Event " << iEvent << " Size = " << photons->size() << std::endl;
     std::vector<int> crystals;
-    for(int eSize = 0 ; eSize < energyDeposition->size() ; eSize++)
+    // std::vector<enDep> EventDepositions;
+    for(int eEvent = 0; eEvent < energyDeposition->size(); eEvent++) //run on energy depositions and find in how many crystals energy was deposited
     {
-      int cry = energyDeposition->at(eSize).CrystalID;
+      //read the crystal where energy was deposited
+      int cry = energyDeposition->at(eEvent).CrystalID;
+      //loop in the crystals found
+      //look for the same id
+      bool sameID = false;
+      for(int j = 0 ; j < crystals.size(); j++)
+      {
+        if(crystals[j] == cry) sameID = true;
+      }
+      if(!sameID) crystals.push_back(cry);
     }
-    // for(int pSize = 0 ; pSize < photons->size() ; pSize++)
-    // {
-    //   std::cout << "GlobalTime = " << photons->at(iEvent).GlobalTime << std::endl;
+
+    //now take only events where energy was deposited in 2 crystals and total energy deposited is 511KeV
+    if((crystals[0] == 27 && crystals[1] == 28) | (crystals[0] == 28 && crystals[1] == 27))
+    // if(true)
+    {
+      if(totalEnergyDeposited > 0.510)
+      {
+        if(crystals.size() == 2)
+        {
+          //consider only non-lateral crystals
+          bool isCandidate = true;
+          for(int eEvent = 0; eEvent < energyDeposition->size(); eEvent++) //run on energy depositions and check is not on borders
+          {
+            bool check_b = energyDeposition->at(eEvent).CrystalI < 2 | energyDeposition->at(eEvent).CrystalI > 5 | energyDeposition->at(eEvent).CrystalJ < 2 | energyDeposition->at(eEvent).CrystalJ > 5;
+            if(check_b)
+            {
+              isCandidate = false;
+            }
+          }
+          if(isCandidate)
+          {
+            //calculate average Z of production in the 2 crystals
+            // foundCandidate++;
+
+            //calculate the measured W
+            int Measured_pmaxID = 0;
+            int Measured_pmax = 0;
+            int Measured_pSecond = 0;
+            int Measured_totalDetCounts = 0;
+            std::vector<int> vToSort;
+            for(int i = 0; i < numOfCh ; i++)
+            {
+              Measured_totalDetCounts+=detector[i];
+              vToSort.push_back(detector[i]);
+            }
+            std::sort (vToSort.begin(),vToSort.end());
+            Measured_pmax = vToSort[vToSort.size()-1];
+            Measured_pSecond = vToSort[vToSort.size()-2];
+
+            // {
+            //   Measured_totalDetCounts += detector[i];
+            //   if(detector[i] > Measured_pmax1)
+            //   {
+            //     Measured_pmax1 = detector[i];
+            //     Measured_pmaxID = i;
+            //   }
+            // }
+
+
+
+            // double Measured_w = ((double) Measured_pmax) / ((double)Measured_totalDetCounts);
+            double Measured_w = ((double) Measured_pmax + (double)Measured_pSecond) / (2.0*(double)Measured_totalDetCounts);
+            double RatioW = 0.0;
+
+
+            std::vector<double> totalEnergyPerCrystal;
+            std::vector<double> averageZ;
+            std::vector<double> wi;
+            std::vector<double> ratio;
+
+            for(int j = 0 ; j < crystals.size() ; j++) // we are in a specific crystal
+            {
+              double temp_totalEnergyPerCrystal = 0.0;
+              for(int eEvent = 0; eEvent < energyDeposition->size(); eEvent++)
+              {
+
+                if(energyDeposition->at(eEvent).CrystalID == crystals[j])
+                temp_totalEnergyPerCrystal += energyDeposition->at(eEvent).EnergyDeposited;
+              }
+
+              double temp_averageZ = 0.0;
+              for(int eEvent = 0; eEvent < energyDeposition->size(); eEvent++)
+              {
+                if(energyDeposition->at(eEvent).CrystalID == crystals[j])
+                temp_averageZ += (energyDeposition->at(eEvent).DepositionZ *  energyDeposition->at(eEvent).EnergyDeposited)/temp_totalEnergyPerCrystal;
+              }
+
+
+
+              // now on opticals
+              // 1.is averageZ correlated to Fi/(Fi+Bi)?
+              // count Fi and Bi for this crystal
+
+              // 2. next, is wi correlated with ratio (actually are they the same?)?
+              // so calculate wi
+              // divide opticals on the basis of origin crystal
+              // count the photons detected by each detector for both origins
+              // compute the two wi
+              long int Fi = 0;
+              long int Bi = 0;
+              short detectorSorted[numOfCh];
+              for(int iDet = 0; iDet < numOfCh ;iDet++)
+              {
+                detectorSorted[iDet] = 0;
+              }
+
+              for(int oEvent = 0; oEvent < photons->size(); oEvent++) //run on energy depositions and find in how many crystals energy was deposited
+              {
+                // calc origin crystal ID
+                int OriginCrystalID = photons->at(oEvent).OriginCrystalI * 8 +  photons->at(oEvent).OriginCrystalJ;
+                if(OriginCrystalID == crystals[j])
+                {
+                  if(photons->at(oEvent).ExitFace == 1)
+                  Fi++;
+                  if(photons->at(oEvent).ExitFace == 2)
+                  Bi++;
+                  //the array of detectors
+                  // std::cout << photons->at(oEvent).PositionX << " "<< (int) ((photons->at(oEvent).PositionX + 6.3) /  3.2) << std::endl;
+                  int iDetector = (int) ((photons->at(oEvent).PositionX + 6.3) /  3.2);
+                  int jDetector = (int) ((photons->at(oEvent).PositionY + 6.3) /  3.2);
+                  int detectorID = iDetector * 4 +  jDetector;
+                  detectorSorted[detectorID]++;
+                }
+              }
+
+              int pmaxID = 0;
+              int pmax = 0;
+              int totalDetCounts = 0;
+              for(int i = 0; i < numOfCh ; i++)
+              {
+                totalDetCounts += detectorSorted[i];
+                if(detectorSorted[i] > pmax)
+                {
+                  pmax = detectorSorted[i];
+                  pmaxID = i;
+                }
+              }
+              double temp_wi = ((double) pmax) / ((double)totalDetCounts);
+
+
+              double temp_ratio = ((double) Fi)/((double) Fi+ (double) Bi);
+              wi.push_back(temp_wi);
+              totalEnergyPerCrystal.push_back(temp_totalEnergyPerCrystal);
+              ratio.push_back(temp_ratio);
+              averageZ.push_back(temp_averageZ);
+
+              // RatioW += (wi * totalEnergyPerCrystal) / totalEnergyDeposited;
+
+
+
+
+            }
+
+            bool doubleAccepted = true;
+            for(int countCry = 0; countCry < wi.size() ;countCry++ )
+            {
+              if(totalEnergyPerCrystal[countCry] < 0.05) doubleAccepted = false;
+            }
+            if(doubleAccepted)
+            {
+              foundCandidate++;
+              for(int countCry = 0; countCry < wi.size() ;countCry++ )
+              {
+                averageZvsRatio->Fill(ratio[countCry],averageZ[countCry]);
+                wivsRatio->Fill(ratio[countCry],wi[countCry]);
+                averageZvsWi->Fill(wi[countCry],averageZ[countCry]);
+                RatioW += (wi[countCry] * totalEnergyPerCrystal[countCry]) / totalEnergyDeposited;
+              }
+              wMeasuredvsRatioW->Fill(RatioW,Measured_w);
+            }
+
+          }
+        }
+      }
+    }
+
+
+
+
+
+
+
+    if(totalEnergyDeposited > 0.510)
+    {
+      if(crystals.size() == 1) singleCounter++;
+      if(crystals.size() == 2) doubleCounter++;
+      if(crystals.size() == 3) tripleCounter++;
+      if(crystals.size() > 3)  multipleCounter++;
+    }
+    // std::cout << "Event " << iEvent << " - Dep in crystals = " << crystals.size() << std::endl;
+
+    // columsum = 0;
+    // rowsum = 0;
+    // total = 0;
+    // floodx = 0;
+    // floody = 0;
+    // floodz = 0;
     //
+    //
+    //
+    // //first clean the array
+    // for(int i = 0; i < 16 ; i++)
+    // {
+    //   charge[i] = 0;
+    // }
+    //
+    // //then fill it with the detector data
+    // for(int i = 0; i < numOfCh ; i++)
+    // {
+    //   charge[i] = detector[i];
+    // }
+    //
+    // //calculate weighted energy and fill 2d histo
+    // for(int i = 0; i < 16 ; i++)
+    // {
+    //   columsum += charge[i]*ymppc[i];
+    //   rowsum += charge[i]*xmppc[i];
+    //   total += charge[i];
+    // }
+    // floodx = rowsum/total;
+    // floody = columsum/total;
+    // b_floodx->Fill();
+    //
+    // flood->Fill(floodx,floody);
+
+
+    //calculate the average x and y deposition position
+    // double averageX = 0;
+    // double averageY = 0;
+    // double energyColumnSum = 0;
+    // double energyRowSum = 0;
+    //
+    // for(int i = 0; i < nCrystals ; i++)
+    // {
+    //   for (int j = 0 ; j < pEdep[i]->size() ; j++)
+    //   {
+    //     energyRowSum    += px[i]->at(j) * pEdep[i]->at(j);
+    //     energyColumnSum += py[i]->at(j) * pEdep[i]->at(j);
+    //   }
+    // }
+    // averageX = energyRowSum / totalEnergyDeposited;
+    // averageY = energyColumnSum / totalEnergyDeposited ;
+    //
+    // positions->Fill(averageX,averageY);
+    //t1->Fill();
+
+
+    // ExtendedTimeTag = 1e-9;
+    // DeltaTimeTag = 1e-9;
+    //
+    // NumbOfInteractions = 0;
+    // CrystalsHit = 0;
+    //
+    //
+    // for(int i = 0; i < numOfCh ; i++)
+    // {
+    //   //convert to ADC channels, as if it was data from a digitizer
+    //   //mppc gain = 1.25e6
+    //   //adc channel binning 156e-15 C
+    //   double adcCh = detector[i]*1.25e6*1.6e-19/156e-15;
+    //   charge[i*2] = (Short_t) adcCh;
+    // }
+    //
+    // RealX = RealY = RealZ = 0;
+    //
+    // // calculate a weigthed energy deposition in x,y,z
+    // for(int i = 0; i < numOfCry ; i++) //first total energy deposited
+    // {
+    //   NumbOfInteractions += px[i]->size();
+    //   if(px[i]->size()) CrystalsHit++;
+    //   for(int j = 0; j < px[i]->size(); j++)
+    //   {
+    //     RealX += (px[i]->at(j) * pEdep[i]->at(j))/totalEnergyDeposited;
+    //   }
+    //   for(int j = 0; j < px[i]->size(); j++)
+    //   {
+    //     RealY += (py[i]->at(j) * pEdep[i]->at(j))/totalEnergyDeposited;
+    //   }
+    //   for(int j = 0; j < px[i]->size(); j++)
+    //   {
+    //     RealZ += (pz[i]->at(j) * pEdep[i]->at(j))/totalEnergyDeposited;
+    //   }
+    // }
+    //
+    // if(NumbOfInteractions > 0) // discard events with no energy deposition (they would never trigger the detectors anyway..)
+    // {
+    //   t1->Fill();
     // }
 
+    counter++;
+
+    int perc = ((100*counter)/nEntries); //should strictly have not decimal part, written like this...
+    if( (perc % 10) == 0 )
+    {
+      std::cout << "\r";
+      std::cout << perc << "% done... ";
+      //std::cout << counter << std::endl;
+    }
+
+
   }
+
+  std::cout << std::endl;
+  std::cout << "1 cry [511 KeV deposition] events = "    << singleCounter << std::endl;
+  std::cout << "2 cry [511 KeV deposition] events = "    << doubleCounter << std::endl;
+  std::cout << "3 cry [511 KeV deposition] events = "    << tripleCounter << std::endl;
+  std::cout << "Multi cry [511 KeV deposition] events = "<< multipleCounter << std::endl;
+  std::cout << "Candidates = "<< foundCandidate << std::endl;
+
+  std::string outFile = "FileOut.root";
+  TFile* fOut = new TFile(outFile.c_str(),"recreate");
+  averageZvsRatio->Write();
+  averageZvsWi->Write();
+  wivsRatio->Write();
+  wMeasuredvsRatioW->Write();
+  // t1->Write();
+
+  //   f1->Close();
+
+  // std::string outFile = "analysis_OUT.root";
+  // TFile* fOut = new TFile(outFile.c_str(),"recreate");
+
+  // flood->Write();
+  // positions->Write();
+  //   f1->Close();
+
+  fOut->Close();
 
 
 
